82c96463a615d9aa730c4bfa842fd56f7365d4b9
shella12 <ayeshaarshad4567@gmail.com>
2022-06-08 23:31:00 +0500

    first commit

--- courses/foundation/lessons/pre/youtube_replica/script.js
+++ courses/foundation/lessons/pre/youtube_replica/script.js
@@ -1,1 +1,7 @@
 
+var menuIcon=document.querySelector(".menu-icon");
+var sidebar=document.querySelector(".side-bar");
+
+menuIcon.onclick=function(){
+    sidebar.classList.toggle("colapsed-sidebar");
+}
